74315202561b42c835edd487ead38cdbadeecb06
Alex Diaz <imloadinq7579@gmail.com>
2025-03-28 15:17:56 -0500

    changes

--- frontend/src/components/gameview/gameview.component.ts
+++ frontend/src/components/gameview/gameview.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';;
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -15,14 +16,45 @@ export class GameviewComponent {
 
   readonly GAMES_URL = 'http://localhost:8080/api/games';
   readonly API_KEY = '9_yInw3N1UQeeRk5qLzqKZzn';
+  readonly PLATFORMS_URL = 'http:/localhost:8080/api/platforms'
 
-  games$: Observable<any> | undefined;
+  games$: Observable<string[]> | undefined;
+  gameDetails$: Observable<any> | undefined;
+  platforms$: Observable<any> | undefined;
 
-  getGames() {
+  getGameTitles() {
     const headers = new HttpHeaders({
       'X-API-KEY': this.API_KEY
     });
 
-    this.games$ = this.http.get(this.GAMES_URL, { headers });
+    this.games$ = this.http.get<any[]>(this.GAMES_URL, { headers }).pipe(
+      map(games => games
+        .sort((a, b) => new Date(a.ReleaseDate).getTime() - new Date(b.ReleaseDate).getTime())
+        .map(game => game.GameTitle)
+      )
+    );
+
+  }
+
+  getGameDetails() {
+    const headers = new HttpHeaders({
+      'X-API-KEY': this.API_KEY
+    });
+
+    this.gameDetails$ = this.http.get<any[]>(this.GAMES_URL, { headers });
+  }
+
+  getPlatforms() {
+    const headers = new HttpHeaders({
+      'X-API-KEY': this.API_KEY
+    });
+
+    this.platforms$ = this.http.get<any[]>(this.PLATFORMS_URL, { headers })
+  }
+
+  clear() {
+    this.games$ = of([]);
+    this.gameDetails$ = of([]);
+    this.platforms$ = of([]);
   }
 }

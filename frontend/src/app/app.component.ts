87dd1e5f2ffff85f298299d000d9b435cf5d21fa
Alex Diaz <imloadinq7579@gmail.com>
2025-03-28 12:55:24 -0500

    added GameviewComponent and HttpClientModeule to imports

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { GameviewComponent } from '../components/gameview/gameview.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GameviewComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

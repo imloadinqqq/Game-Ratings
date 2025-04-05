d63e9351ec9fda7b5b0c76ae9bfbd84602bd143b
Alex Diaz <imloadinq7579@gmail.com>
2025-04-05 16:32:14 -0500

    added imports for RouterLink, added routes to file

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { GameviewComponent } from '../components/gameview/gameview.component';
+import { LoginComponent } from '../components/login/login.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'home', component: GameviewComponent, title: 'Home' },
+  { path: 'login', component: LoginComponent, title: 'Login' }
+];

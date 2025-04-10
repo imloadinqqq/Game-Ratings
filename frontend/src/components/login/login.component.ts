d30ee42f431aa121f12c4ef3a5c0483df8b15813
Alex Diaz <imloadinq7579@gmail.com>
2025-04-09 22:42:19 -0500

    style login page

--- frontend/src/components/login/login.component.ts
+++ frontend/src/components/login/login.component.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  submitUserInfo() {
+    console.log("working");
+  }
 }

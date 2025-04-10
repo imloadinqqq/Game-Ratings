c90945296009f0e6ab95aa449f4379e431c9a221
Alex Diaz <imloadinq7579@gmail.com>
2025-04-10 11:51:30 -0500

    "show/hide" button visibility logic

--- frontend/src/components/login/login.component.ts
+++ frontend/src/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -11,8 +11,26 @@ import { RouterModule } from '@angular/router';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  @ViewChild('pass') password!: ElementRef;
+  typedPassword: boolean = false;
+  show: boolean = false;
+
+  ngAfterViewInit() {
+    this.typedPassword = this.password.nativeElement.value.length > 0;
+  }
 
   submitUserInfo() {
     console.log("working");
+  }
+
+  showPassword() {
+    this.show = !this.show;
+    this.password.nativeElement.type = this.show ? 'text' : 'password';
+  }
+
+  editPassword() {
+    const input = this.password.nativeElement.value;
+    this.typedPassword = input.length > 0;
+    console.log(this.typedPassword);
   }
 }

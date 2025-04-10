b3d4c93aa6c514fdf0b62f4d54727bf8113cf523
Alex Diaz <imloadinq7579@gmail.com>
2025-04-10 17:16:12 -0500

    added new createUser route, removed HttpClientModule (deprecated)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, HttpClientModule],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
